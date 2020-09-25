2c9e8e3de1ebf565490ea38a1cc4c541c2a01e26
FominVlad <mr.vladyslavfomin@gmail.com>
2020-09-25 15:23:59 +0300

    Add template MVC project. Add Chip model

---
+++ Lab1-2/GameDev/GameDev/Models/Chip.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameDev.Models
+{
+    public enum ChipColor
+    {
+        White = 0,
+        Black = 1,
+        Gold = 2,
+        Silver = 3
+    }
+
+    public class Chip
+    {
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+
+        public ChipColor Color { get; set; }
+
+        public Chip(int posX, int posY, ChipColor color)
+        {
+            this.PosX = posX;
+            this.PosY = posY;
+            this.Color = color;
+        }
+    }
+}
